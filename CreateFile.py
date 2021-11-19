1848b314dd037ce8a67a0a429dfe50e874c699d9
SrinivasaRao A <asraotuni@users.noreply.github.com>
2021-11-19 18:48:16 +0530

    file path updated

--- CreateFile.py
+++ CreateFile.py
@@ -1,5 +1,5 @@
 print ("hello srinivas")
 
-outputFile = open("C:/MyOneDrive/OneDrive - Cloud Valley (Comm-IT)/Documents/Work-Scribble/output.txt", "w")
+outputFile = open("/home/ec2-user/mat-output.txt", "w")
 outputFile.write("Boker Tov Comm-IT")
 outputFile.close
