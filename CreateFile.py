94a22a6aa8705fd2a9a0d84dac286715f19aeebb
SrinivasaRao A <asraotuni@users.noreply.github.com>
2021-11-19 18:40:56 +0530

    test file added

---
+++ CreateFile.py
@@ -0,0 +1,5 @@
+print ("hello srinivas")
+
+outputFile = open("C:/MyOneDrive/OneDrive - Cloud Valley (Comm-IT)/Documents/Work-Scribble/output.txt", "w")
+outputFile.write("Boker Tov Comm-IT")
+outputFile.close
